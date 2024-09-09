6f1478a2f566d98201d200f02fe57536879c29fa
Onur Oztas <github@onuroztas.com>
2024-09-09 10:27:39 +0300

    Remove test endpoints from UserController
    - Deleted /api/users/test, /test, and /api/users/secured endpoints
    - Cleaned up UserController for production use

--- src/main/java/com/onuroztas/expensetracker/controller/UserController.java
+++ src/main/java/com/onuroztas/expensetracker/controller/UserController.java
@@ -4,7 +4,6 @@ import com.onuroztas.expensetracker.model.User;
 import com.onuroztas.expensetracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,21 +19,6 @@ public class UserController {
     public ResponseEntity<String> registerUser(@RequestBody User user) {
         userService.registerNewUser(user.getUsername(), user.getPassword(), user.getEmail());
         return ResponseEntity.ok("User registered successfully");
-    }
-
-    @GetMapping("/api/users/test")
-    public ResponseEntity<String> apiTestEndpoint(Authentication authentication) {
-        return ResponseEntity.ok("Hello, " + authentication.getName() + "! API Test endpoint working");
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<String> testEndpoint(Authentication authentication) {
-        return ResponseEntity.ok("Hello, " + authentication.getName() + "! Root Test endpoint working");
-    }
-
-    @GetMapping("/api/users/secured")
-    public ResponseEntity<String> securedEndpoint(Authentication authentication) {
-        return ResponseEntity.ok("Hello, " + authentication.getName() + "! This is a secured endpoint.");
     }
 
     @GetMapping("/api/users/all")
