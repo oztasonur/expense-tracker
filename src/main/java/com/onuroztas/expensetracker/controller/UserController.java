8651eb659c9cb7d8ce5e299268503cd7727109dc
Onur Öztaş <github@onuroztas.com>
2024-11-14 07:50:13 +0300

    Update CrossOrigin annotation to new localhost port
    - Changed @CrossOrigin annotation from "http://localhost:3000" to "http://localhost:5173" to reflect the updated development server port.

--- src/main/java/com/onuroztas/expensetracker/controller/UserController.java
+++ src/main/java/com/onuroztas/expensetracker/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 
 @RestController
 @RequestMapping("api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     @Autowired
